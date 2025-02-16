9b1beed973f4dede8d9b85aa241e9dbeb3c72979
Zubairical <167015585+Zubairical@users.noreply.github.com>
2025-02-16 05:55:56 +0500

    Add files via upload

---
+++ check_date.php
@@ -0,0 +1,18 @@
+<?php
+// check_date.php
+header('Content-Type: application/json');
+
+// Manually set your cutoff date in "Y-m-d" format.
+// Update this date manually when you want the redirection to occur.
+$cutoffDate = "2025-02-16";
+
+// Get today's date.
+$today = date("Y-m-d");
+
+// If today's date is on or after the cutoff date, signal a redirect.
+if ($today >= $cutoffDate) {
+    echo json_encode(["redirect" => true]);
+} else {
+    echo json_encode(["redirect" => false]);
+}
+?>
